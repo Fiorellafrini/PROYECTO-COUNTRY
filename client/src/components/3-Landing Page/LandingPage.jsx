b9f529e7eb98eed724928a157568e30518c459e3
fiorella frini <fiorella_frini@hotmail.com>
2023-03-03 15:41:35 -0300

    Front

--- client/src/components/3-Landing Page/LandingPage.jsx
+++ client/src/components/3-Landing Page/LandingPage.jsx
@@ -4,20 +4,49 @@ import styles from "./LandingPage.module.css"
 
 const LandingPage = () => {
     return (
-        <div className={styles.bodyLanding}>
-           <div className={styles.title}>
-            To travel the world!
-            </div>
-          <div className={styles.land}>
-            <Link  to="/home">
-              <button>HOME</button>
-            </Link>
+      
+      <div className={styles.bodyLanding}>
+      {/* <video autoPlay muted loop className={styles.vid}> */}
+        {/* <source src="videolanding" type="video/mp4"></source> */}
+      {/* </video> */}
+       <div className={styles.title1}>           
+        Welcome 
+         {/* To travel the world! */}
+     </div>
 
-            <Link to="/about">
-              <button>ABOUT</button>
-            </Link>
-            </div>
+     <div className={styles.title2}>
+         to the 
+      <br></br>
+         world of travel!
+         {/* To travel the world! */}
+     </div>
+
+        <div className={styles.land}>
+        <Link  to="/home">
+          <button>HOME</button>
+        </Link>
+
+        <Link to="/about">
+          <button>ABOUT</button>
+        </Link>
         </div>
+    
+    
+  </div>
+        // <div className={styles.bodyLanding}>
+        //    <div className={styles.title}>
+        //     To travel the world!
+        //     </div>
+        //   <div className={styles.land}>
+        //     <Link  to="/home">
+        //       <button>HOME</button>
+        //     </Link>
+
+        //     <Link to="/about">
+        //       <button>ABOUT</button>
+        //     </Link>
+        //     </div>
+        // </div>
             
     )
 }
